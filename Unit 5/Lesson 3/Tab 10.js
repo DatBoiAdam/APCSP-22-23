09f0dc5caea7088728a5d393de3d86fb48928197
DatBoiAdam <99695384+DatBoiAdam@users.noreply.github.com>
2023-01-22 22:43:22 -0500

    Create Tab 10.js

---
+++ Unit 5/Lesson 3/Tab 10.js
@@ -0,0 +1,19 @@
+var animals = ["cat", "dog", 6, "bird", "raccoon", "bear", 11, "rabbit", "coyote", "deer", 13, "buffalo", "lizard", "turtle"];
+removeItem(animals, 2);
+
+// Add code to remove the other two numbers from the list
+removeItem(animals, 5);
+removeItem(animals, 8);
+
+console.log(animals);
+
+
+
+var months = ["January", "February", "March", "May", "June", "July", "August", "October", "December"];
+insertItem(months, 3, "April");
+
+// Add code to insert the months of "September" and "November" to their correct positions in the list.
+insertItem(months, 10, "November");
+insertItem(months, 8, "September");
+
+console.log(months);
