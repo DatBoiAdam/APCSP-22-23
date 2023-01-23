3b47f21037cc174da4b7e04d95d5fc46bd591f10
DatBoiAdam <99695384+DatBoiAdam@users.noreply.github.com>
2023-01-22 22:33:27 -0500

    Create Part B.js

---
+++ Unit 5/Lesson 7/Tab 4/Part B.js
@@ -0,0 +1,15 @@
+// Create and assign variable based on user input
+var counter = promptNum("Enter a number");
+
+//TO DO #1: Update the conditional statement so that the loop runs
+//as long as the counter is greater than 0
+while((counter > 0)){
+  //TO DO #2: Add code to print the number in the countdown and
+  //then decrease the value of the counter by 1 each time
+  console.log(counter);
+  counter--;
+}
+
+//TO DO #3: Add code to print "Blastoff!" after the loop is done running
+console.log("BLASTOFF!!!!")
+
