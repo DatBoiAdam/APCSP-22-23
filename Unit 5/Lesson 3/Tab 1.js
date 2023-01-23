338d515b22d767f50f36294b08a989c82da1b433
DatBoiAdam <99695384+DatBoiAdam@users.noreply.github.com>
2023-01-22 22:39:42 -0500

    Create Tab 1.js

---
+++ Unit 5/Lesson 3/Tab 1.js
@@ -0,0 +1,12 @@
+var myLuckyNumbers = [63, 74, 98, 33];
+console.log("My Lucky Numbers:");
+console.log(myLuckyNumbers);
+
+// create a list of four numbers
+var myUnluckyNumbers = ["69", "420", "21", "7"];
+
+
+console.log("My UnluckyNumbers:");
+console.log(myUnluckyNumbers);
+
+// print the list to the console
