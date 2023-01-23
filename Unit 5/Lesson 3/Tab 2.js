f42ffebda9e87b82b9023367cbbdbe48eddf2696
DatBoiAdam <99695384+DatBoiAdam@users.noreply.github.com>
2023-01-22 22:40:05 -0500

    Create Tab 2.js

---
+++ Unit 5/Lesson 3/Tab 2.js
@@ -0,0 +1,10 @@
+console.log("Trees:");
+var trees = ["oak", "maple", "dogwood"];
+console.log(trees);
+console.log("");
+
+console.log("My Favorite Songs:");
+// create a list with three of your favorite songs
+var weezer = ["Buddy Holley", "Pink Triangle", "In the Garage"];
+// print the list to the console
+console.log(weezer);
