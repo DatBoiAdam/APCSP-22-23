e4673212bfc80b612bb372816b2e00b3277c0925
DatBoiAdam <99695384+DatBoiAdam@users.noreply.github.com>
2023-01-22 22:42:03 -0500

    Create Tab 8.js

---
+++ Unit 5/Lesson 3/Tab 8.js
@@ -0,0 +1,19 @@
+var food = ["carrots", "pizza", "lettuce", "strawberries"];
+console.log("My food list: " + food);
+console.log("Length of list: " + food.length);
+
+appendItem(food, "peanuts");
+console.log("My food list: " + food);
+console.log("Length of list: " + food.length);
+
+// use appendItem() to add an item to the food list
+
+appendItem(food, "chicken");
+console.log("My food list: " + food);
+console.log("Length of list: " + food.length);
+
+// use appendItem() to add an item to the food list
+
+appendItem(food, "burger");
+console.log("My food list: " + food);
+console.log("Length of list: " + food.length);
