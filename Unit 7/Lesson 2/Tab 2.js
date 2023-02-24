e4677d2fa50c8e8f1a0b69321743f9cc76862292
DatBoiAdam <99695384+DatBoiAdam@users.noreply.github.com>
2023-02-24 13:55:26 -0500

    Create Tab 2.js

---
+++ Unit 7/Lesson 2/Tab 2.js
@@ -0,0 +1,34 @@
+var wordList = getColumn("Words", "Word");
+var filteredWordList = [];
+
+filter(getNumber("lengthDropdown"), getText("letterDropdown").toLowerCase());
+
+onEvent("lengthDropdown", "change", function(){
+  filter(getNumber("lengthDropdown"), getText("letterDropdown").toLowerCase());
+});
+
+onEvent("letterDropdown", "change", function(){
+  filter(getNumber("lengthDropdown"), getText("letterDropdown").toLowerCase());
+});
+
+
+function filter(len, letter){
+  showElement("waitingImage");
+  showElement("unclickable");
+  filteredWordList = [];
+  setText("output", "");
+  
+  for(var i=0; i<wordList.length; i++){
+    if(wordList[i].length == len && wordList[i].substring(0,getText("letterDropdown").length)==letter){
+        appendItem(filteredWordList, wordList[i]);
+    } 
+  }
+  
+  if(filteredWordList.length == 0){
+    appendItem(filteredWordList, "No Options Available");
+  }
+  
+  hideElement("waitingImage");
+  hideElement("unclickable");
+  setText("output", filteredWordList.join(", "));
+}
